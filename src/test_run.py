ec9191e651b7726890f2eca740940c848e07ef7a
trucnguyen22 <trucnguyenytc@gmail.com>
2026-07-09 12:59:15 -0700

    first commit

---
+++ src/test_run.py
@@ -0,0 +1,7 @@
+import mnist_loader
+import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3.0, test_data=test_data)
